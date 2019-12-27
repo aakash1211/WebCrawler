02387c4d1bee7f52f96c287a8135d235936d5413
Code Zeus <44020809+aakash1211@users.noreply.github.com>
2019-12-28 00:02:59 +0530

    Add files via upload

---
+++ com.work/src/com/work/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.work.controller;
+
+public class LoginController {
+
+	
+	public static void main(String [] args) {
+		System.out.println("hello world");
+	}
+}
---
+++ com.work/src/com/work/model/User.java
@@ -0,0 +1,7 @@
+package com.work.model;
+
+@Entity
+public class User {
+
+	
+}
